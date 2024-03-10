45da8a5a9e826b954781e6c947d55f75bb8b705f
“Ana <anajbritos@gmail.com>
2024-03-10 13:13:21 -0300

    Inicializacao do projeto, configuracoes de virtualizacao e conexao do banco de dados

---
+++ src/main/java/io/ana/julia/iusafe/IUSafeApplication.java
@@ -0,0 +1,13 @@
+package io.ana.julia.iusafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IUSafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IUSafeApplication.class, args);
+    }
+
+}
